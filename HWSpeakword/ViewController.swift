de7d9ba80d548784741cc4944cdec21a21b74e3b
Daniel <babipapi@gmail.com>
2019-04-13 17:37:19 +0800

    fix1

--- HWSpeakword/ViewController.swift
+++ HWSpeakword/ViewController.swift
@@ -7,14 +7,31 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var wordTextField: UITextField!
+    @IBOutlet weak var toneSlider: UISlider!
+    @IBOutlet weak var speedSlider: UISlider!
     override func viewDidLoad() {
+    
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    @IBAction func speakButtom(_ sender: Any) {
+        let word = AVSpeechUtterance(string: wordTextField.text!)
+        let voice = AVSpeechSynthesizer()
+        word.pitchMultiplier = toneSlider.value
+        word.rate = speedSlider.value
+        voice.speak(word)
+    }
+    
+    @IBAction func toneAction(_ sender: Any) {
+        
+    }
+    
+    @IBAction func speedAction(_ sender: Any) {
+        
+    }
 }
 
